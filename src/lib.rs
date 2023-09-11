7e52e056fe8f1684bda2dcadb2a121318e802c94
Bill Avery <wravery@gmail.com>
2023-09-11 08:03:30 -0700

    Remove default test created by cargo new

--- src/lib.rs
+++ src/lib.rs
@@ -528,18 +528,3 @@ impl DeferCallbackQueue {
         }
     }
 }
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
